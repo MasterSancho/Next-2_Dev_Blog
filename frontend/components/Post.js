675408932fed207cc502dbd1ac5cf92a0a13dbc4
Alex M <1maltsevalex@gmail.com>
2021-08-08 15:19:36 +0300

    10. Post component-2

--- frontend/components/Post.js
+++ frontend/components/Post.js
@@ -4,6 +4,16 @@ export default function Post({ post }) {
  return (
   <div className='card'>
    <img src={post.frontmatter.cover_image} alt='' />
+
+   <div className='post-date'>Posted on {post.frontmatter.date}</div>
+
+   <h3>{post.frontmatter.title}</h3>
+
+   <p>{post.frontmatter.excerpt}</p>
+
+   <Link href={`/blog/${post.slug}`}>
+    <a className='btn'>Read More</a>
+   </Link>
   </div>
  );
 }
