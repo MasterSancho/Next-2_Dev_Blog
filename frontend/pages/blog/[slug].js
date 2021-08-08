afce3331408574b1503ab49734d4a04217100a7f
Alex M <1maltsevalex@gmail.com>
2021-08-08 16:03:30 +0300

    13. dangerouslySetInnerHTML

--- frontend/pages/blog/[slug].js
+++ frontend/pages/blog/[slug].js
@@ -9,7 +9,21 @@ export default function PostPage({
  slug,
  content,
 }) {
- return <div>{title}</div>;
+ return (
+  <>
+   <Link href='/'>
+    <a className='btn btn-back'>Go Back</a>
+   </Link>
+   <div className='card card-page'>
+    <h1 className='post-title'>{title}</h1>
+    <div className='post-date'>Posted on {date}</div>
+    <img src={cover_image} alt='' />
+    <div className='post-body'>
+     <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+    </div>
+   </div>
+  </>
+ );
 }
 
 export async function getStaticPaths() {
